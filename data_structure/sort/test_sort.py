793104eac3f30c650b07e008a4bfd115a931006e
linxinzhe <linxinzhechn@icloud.com>
2018-01-03 10:54:09 +0800

    feat(sort): quick sort

--- data_structure/sort/test_sort.py
+++ data_structure/sort/test_sort.py
@@ -1,7 +1,9 @@
 import unittest
+
 from .bubble_sort import bubble_sort
-from .select_sort import select_sort
 from .insert_sort import insert_sort
+from .quick_sort import quick_sort
+from .select_sort import select_sort
 from .shell_sort import shell_sort
 
 
@@ -48,3 +50,36 @@ class TestShellSort(TestBubbleSort):
     def setUp(self):
         super().setUp()
         self.list = shell_sort
+
+
+class TestQuickSort(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.sort = quick_sort
+
+    def test_sort(self):
+        a = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
+
+    def test_sort_0(self):
+        a = []
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
+
+    def test_sort_1(self):
+        a = [54]
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
+
+    def test_sort_2(self):
+        a = [11, 22]
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
+        a = [22, 11]
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
+        a = [11, 11]
+        self.sort(a, 0, len(a) - 1)
+        self.assertListEqual(sorted(a), a)
